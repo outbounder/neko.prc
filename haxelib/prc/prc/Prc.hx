02f3e5ad4344e98b0bce782e93d8bf57ba0c02e1
outbounder <obiwonn@gmail.com>
2009-11-19 17:22:42 +0200

    creating haxe lib

---
+++ haxelib/prc/prc/Prc.hx
@@ -0,0 +1,13 @@
+package prc;
+
+class Prc
+{
+	public function new() { }
+	
+	public function exec(cmd:String):String
+	{
+		return _exec(untyped cmd.__s);
+	}
+	
+	static var _exec = neko.Lib.load("Prc","exec",1);
+}
